10e97189557e0ceb5f3b5cc14d2fc9a3cd24566a
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2024-01-14 09:47:03 -0300

    Possibilidades de uso do data-binding

--- app/Livewire/HelloWorld.php
+++ app/Livewire/HelloWorld.php
@@ -7,6 +7,9 @@ use Livewire\Component;
 class HelloWorld extends Component
 {
     public $name = 'Darlinton Luis';
+    public $salutation = 'Hello';
+    public $color = 'white';
+    public $salutationOptions = ['Hello', 'Bye Bye'];
 
     public function render()
     {
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,4 +1,10 @@
-<div style="border: 1px solid red; padding: 32px">
-    <h2> Hello, {{ $name }}</h2>
-    <input type="text" wire:model="name" />
+<div style="border: 1px solid red; padding: 32px; background-color: {{ $color }}">
+    <h1> {{ $salutation }}, {{ strtoupper($name) }}</h1>
+    <select wire:model.live="salutation">
+        @foreach($salutationOptions as $option)
+            <option value="{{ $option }}">{{ $option }}</option>
+        @endforeach
+    </select>
+
+    <input type="color" wire:model.live="color" />
 </div>
